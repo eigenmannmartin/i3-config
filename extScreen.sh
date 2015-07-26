8f1452d3b3e6183ef363c2971452433c4e3a1246
Martin Eigenmann <git@eigenmannmartin.ch>
2015-07-26 18:07:52 +0200

    add shortcuts for monitor-setup

---
+++ extScreen.sh
@@ -0,0 +1,3 @@
+xrandr --output HDMI1 --auto
+xrandr --output eDP1 --off
+feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
---
+++ intScreen.sh
@@ -0,0 +1,3 @@
+xrandr --output eDP1 --mode 1920x1080
+xrandr --output HDMI1 --off
+feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
